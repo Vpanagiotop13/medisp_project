bd1ec61d40fc610aba556937d56c2457a02daf37
Vasilis Panagiotop <vpanagiotop@uniwa.gr>
2023-03-29 19:47:36 +0300

    new

--- medisp_project/settings.py
+++ medisp_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "machine_learning.apps.MachineLearningConfig",
+    "django_extensions",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
